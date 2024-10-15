b869beddf50bb7e1ebb0b379a35656fef0f1cd6d
EngineerMoore <alexiashalise@gmail.com>
2024-10-15 11:08:08 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Froyo Order Tracker
+## Objective
+Create an app for a frozen yogurt shop that is able to keep track of the type and number of froyo orders they receive
---
+++ index.js
@@ -0,0 +1,8 @@
+/* Create a prompt where users list out froyo flavors seperated by commas 
+Ex. "vanilla,vanilla,vanilla,strawberry,coffee,coffee."*/
+// Seperate the input into an array of strings
+// Loop through through the array to find the flavors(?)
+// Create an object that lists how many of each flavor was ordered
+// Create a function to that returns an object
+  // the function should contain the array used to count flavor events
+// Make the object visible on the console
