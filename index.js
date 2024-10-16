d29d9807eaa6909f7c572f5c03f39d5bf296f694
EngineerMoore <alexiashalise@gmail.com>
2024-10-16 01:02:40 -0500

    updated index.js file w/ userInput's flavors

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
 /* Create a prompt where users list out froyo flavors seperated by commas 
 Ex. "vanilla,vanilla,vanilla,strawberry,coffee,coffee."*/
+const userInput = prompt( `Please enter your desired flavors. Be sure to seperate each flavor using commas`, `vanilla,vanilla,vanilla,strawberry,coffee,coffee`)
+
 // Seperate the input into an array of strings
-// Loop through through the array to find the flavors(?)
-// Create an object that lists how many of each flavor was ordered
-// Create a function to that returns an object
-  // the function should contain the array used to count flavor events
-// Make the object visible on the console
+const ordersRaw = userInput.split(`,`);
+// console.log(ordersRaw)
+
+ordersRaw.sort()
+// console.log(ordersRaw)
+
+const flavorOneArray = []
+const flavorOneIndex = 0
+let flavorOne = ordersRaw[0];
+for (let i = 0; ordersRaw[i]===flavorOne; i++){{flavorOneArray.push(ordersRaw[i])}}
+console.log(flavorOneArray) 
+
+const flavorTwoArray = []
+const flavorTwoIndex = flavorOneIndex + flavorOneArray.length;
+const flavorTwo = ordersRaw[flavorTwoIndex];
+for (let i = 0; i < ordersRaw.length; i++)
+  {if (ordersRaw[i]===flavorTwo)
+     {flavorTwoArray.push(ordersRaw[flavorTwoIndex])}
+  }
+console.log(flavorTwoArray)
+
+const flavorThreeArray = []
+const flavorThreeIndex = flavorTwoIndex + flavorTwoArray.length;
+const flavorThree = ordersRaw[flavorThreeIndex]
+for (let i = 0; i < ordersRaw.length; i++)
+{if (ordersRaw[i]===flavorThree)
+{flavorThreeArray.push(ordersRaw[flavorThreeIndex])}
+}
+console.log(flavorThreeArray)
+
+
+
+
+const froyoOrders = {
+  vanilla: flavorThreeArray.length,
+  stawberry: flavorTwoArray.length,
+  coffee: flavorOneArray.length
+  }
+
+  console.log(froyoOrders)
+
+
+  
+
+
+
+
+
+
+
+       
